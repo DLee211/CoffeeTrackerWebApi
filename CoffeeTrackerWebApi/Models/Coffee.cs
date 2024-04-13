5b19fd439af229cf6c410e317a1f2a0764a2df5f
DLee211 <90346764+DLee211@users.noreply.github.com>
2024-04-13 18:43:54 -0400

    Initial commit

---
+++ CoffeeTrackerWebApi/Models/Coffee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeTrackerWebApi.Models;
+
+public class Coffee
+{
+    public int Id { get; set; }
+    [Required]
+    public DateTime DateConsumed { get; set; }
+    [Required]
+    public int Cups { get; set; }
+    [MaxLength(200)]
+    public string Notes { get; set; }
+}
